679a810a6c222abde29ed35cf741039c76154bbd
fernando-eb2406 <feb-332@hotmail.com>
2022-03-11 23:46:39 -0300

    DH Heroes completo

---
+++ app.js
@@ -0,0 +1,48 @@
+const express = require("express");
+const path = require("path");
+const app = express();
+const port = 3030;
+
+app.use(express.static(path.join(__dirname, "public")));
+
+app.get("/", (req, res) => {
+  return res.sendFile(path.join(__dirname, "views", "index.html"));
+});
+
+app.get("/home", (req, res) => {
+  return res.sendFile(path.join(__dirname, "views", "index.html"));
+});
+
+app.get("/babbage", (req, res) => {
+  return res.sendFile(path.join(__dirname, "views", "babbage.html"));
+});
+
+app.get("/berners-lee", (req, res) => {
+  return res.sendFile(path.join(__dirname, "views", "berners-lee.html"));
+});
+
+app.get("/clarke", (req, res) => {
+  return res.sendFile(path.join(__dirname, "views", "clarke.html"));
+});
+
+app.get("/hamilton", (req, res) => {
+  return res.sendFile(path.join(__dirname, "views", "hamilton.html"));
+});
+
+app.get("/hopper", (req, res) => {
+  return res.sendFile(path.join(__dirname, "views", "hopper.html"));
+});
+
+app.get("/lovelace", (req, res) => {
+  return res.sendFile(path.join(__dirname, "views", "lovelace.html"));
+});
+
+app.get("/turing", (req, res) => {
+  return res.sendFile(path.join(__dirname, "views", "turing.html"));
+});
+
+app.get("*", (req, res) => {
+  return res.sendFile(path.join(__dirname, "views", "notFound.html"));
+});
+
+app.listen(port, () => console.log(`Server running on port http://localhost:${port}`));
